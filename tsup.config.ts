d135a1e7c1caf6f91eca1e6da2832643fc5010b1
Wang Yang <i@wymoe.com>
2025-08-01 11:34:44 +0800

    feat: 添加 glob 作为开发依赖，并更新 tsup 配置注释

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsup'
+// import { glob } from 'glob'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  // entry: await glob('src/*.ts'), // 使用 glob 匹配 src 目录下的所有 .ts 文件
+  entry: ['src/index.ts'], // 入口文件
   format: ['esm'],   // 只出 ESM
-  dts: true,
-  clean: true,
+  dts: true, // 生成类型定义文件
+  clean: true, // 清理输出目录
+  treeshake: true, // 启用树摇优化
+  splitting: false,  // 不开拆包
+  minify: true,
 })
